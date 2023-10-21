11ae80db9f50a760cbf396463bb88ca7b9857c2c
Lunar Phạm <truong.pham210214@vnuk.edu.vn>
2023-10-21 22:54:20 +0700

    Add API method

--- script.js
+++ script.js
@@ -10,30 +10,18 @@ const showAllButton = document.getElementById('showAll');
 const showOngoingButton = document.getElementById('showOngoing');
 const showCompletedButton = document.getElementById('showCompleted');
 
-// Load tasks from local storage when the page loads
+const API_BASE_URL = 'https://b5241578-0511-44d5-84f1-2b6038167b57.mock.pstmn.io'; // Replace with your API URL
+
+// Load tasks from the API when the page loads
 document.addEventListener('DOMContentLoaded', () => {
-    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    savedTasks.forEach(taskData => {
-        addTaskToList(taskData.text, taskData.completed);
-    });
+    fetch(`${API_BASE_URL}/tasks`)
+        .then(response => response.json())
+        .then(tasks => {
+            tasks.forEach(taskData => {
+                addTaskToList(taskData);
+            });
+        });
 });
-
-// Function to save tasks to local storage
-function saveTasksToLocalStorage() {
-    const tasks = [];
-
-    // Select all the task items on the page
-    const taskItems = Array.from(document.querySelectorAll('li'));
-
-    taskItems.forEach(taskItem => {
-        const text = taskItem.querySelector('.todo-content').textContent;
-        const completed = taskItem.classList.contains('completed');
-
-        tasks.push({ text, completed });
-    });
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
 
 // Add Todo button click event
 addTodoButton.addEventListener('click', () => {
@@ -43,35 +31,16 @@ addTodoButton.addEventListener('click', () => {
 // Add Todo
 addTodo.addEventListener('click', () => {
     const todoText = todoInput.value.trim();
-    if (todoText !== '') {   // In case input is not empty
-        addTaskToList(todoText, false); // New tasks are marked as not completed
-        todoInput.value = '';
-        addTodoModal.style.display = 'none'; // Close modal when click 
+    if (todoText !== '') {
+        createTask({ text: todoText, completed: false })
+            .then(taskData => {
+                addTaskToList(taskData);
+                todoInput.value = '';
+                addTodoModal.style.display = 'none'; // Close modal when clicked
+            })
+            .catch(error => console.error('Error adding task:', error));
     }
 });
-
-// Function to add a task to the list
-function addTaskToList(taskText, completed) {
-    const li = document.createElement('li');
-    li.innerHTML = `
-        <div class="left-content flex">
-            <div class="status-label"></div>
-            <span class="todo-content text-center">${taskText}</span>
-        </div>
-        
-        <div class="button-combo">
-            <button class="edit bg-gray text-white square-button font-bold"><img src="icons/edit.svg" alt="edit" class="svg-icon"></button>
-            <button class="check bg-done text-white square-button font-bold"><img src="icons/check.svg" alt="check" class="svg-icon"></button>
-            <button class="delete bg-danger text-white square-button font-bold"><img src="icons/delete.svg" alt="delete" class="svg-icon"></button>
-        </div>
-    `;
-    if (completed) {
-        li.classList.add('completed');
-    }
-    todoList.appendChild(li);
-    addEventListeners(li);
-    saveTasksToLocalStorage();
-}
 
 // Close modal buttons
 document.querySelectorAll('.close').forEach(closeBtn => {
@@ -81,47 +50,115 @@ document.querySelectorAll('.close').forEach(closeBtn => {
     });
 });
 
+window.addEventListener('click', (event) => {
+    if (event.target === addTodoModal || event.target === editTodoModal) {
+        addTodoModal.style.display = 'none';
+        editTodoModal.style.display = 'none';
+    };
+});
 
-let currentEditingTask = null; // Keep track of the task being edited
+// Function to add a task to the list
+function addTaskToList(taskData) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <div class="left-content flex">
+            <div class="status-label"></div>
+            <span class="todo-content text-center">${taskData.text}</span>
+        </div>
+        
+        <div class="button-combo">
+            <button class="edit bg-gray text-white square-button font-bold"><img src="icons/edit.svg" alt="edit" class="svg-icon"></button>
+            <button class="check bg-done text-white square-button font-bold"><img src="icons/check.svg" alt="check" class="svg-icon"></button>
+            <button class="delete bg-danger text-white square-button font-bold"><img src="icons/delete.svg" alt="delete" class="svg-icon"></button>
+        </div>
+    `;
+    if (taskData.completed) {
+        li.classList.add('completed');
+    }
+    todoList.appendChild(li);
+    addEventListeners(li, taskData);
+}
+
+// Function to create a task
+function createTask(taskData) {
+    return fetch(`${API_BASE_URL}/tasks`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(taskData),
+    }).then(response => response.json());
+}
+
+// Edit button click event
+function editTask(taskData, updatedText) {
+    return fetch(`${API_BASE_URL}/tasks/${taskData.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...taskData, text: updatedText }),
+    }).then(response => response.json());
+}
+
+// Delete button click event
+function deleteTask(taskData) {
+    return fetch(`${API_BASE_URL}/tasks/${taskData.id}`, {
+        method: 'DELETE',
+    });
+}
 
 // Add event listeners to edit, check, and delete buttons
-function addEventListeners(li) {
+function addEventListeners(li, taskData) {
     const editButton = li.querySelector('.edit');
     const checkButton = li.querySelector('.check');
     const deleteButton = li.querySelector('.delete');
 
     editButton.addEventListener('click', () => {
-        currentEditingTask = li; // Set the current editing task
-        editTodoInput.value = li.querySelector('.left-content span').textContent;
+        editTodoInput.value = taskData.text;
         editTodoModal.style.display = 'block';
+
+        saveTodo.addEventListener('click', () => {
+            const updatedText = editTodoInput.value.trim();
+            if (updatedText !== '') {
+                editTask(taskData, updatedText)
+                    .then(updatedTask => {
+                        taskData.text = updatedTask.text;
+                        li.querySelector('span').textContent = updatedTask.text;
+                        editTodoInput.value = ''; // Clear the input field
+                        editTodoModal.style.display = 'none';
+                    })
+                    .catch(error => console.error('Error updating task:', error));
+            }
+        });
     });
 
-    checkButton.addEventListener('click', function () {
-        const listItem = this.closest('li');
-        listItem.classList.toggle('completed');
-        saveTasksToLocalStorage();
+    checkButton.addEventListener('click', () => {
+        if (!taskData.completed) {
+            taskData.completed = true;
+            li.classList.add('completed');
+        } else {
+            taskData.completed = false;
+            li.classList.remove('completed');
+        }
+
+        fetch(`${API_BASE_URL}/tasks/${taskData.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(taskData),
+        }).catch(error => console.error('Error updating task:', error));
     });
 
     deleteButton.addEventListener('click', () => {
-        li.remove();
-        saveTasksToLocalStorage();
+        deleteTask(taskData)
+            .then(() => {
+                li.remove();
+            })
+            .catch(error => console.error('Error deleting task:', error));
     });
 }
-
-// Save button click event in the edit popup modal
-const saveButton = editTodoModal.querySelector('#saveTodo');
-saveButton.addEventListener('click', () => {
-    if (currentEditingTask) {
-        const updatedText = editTodoInput.value.trim();
-        if (updatedText !== '') {
-            currentEditingTask.querySelector('span').textContent = updatedText;
-            editTodoInput.value = ''; // Clear the input field
-            editTodoModal.style.display = 'none';
-            currentEditingTask = null; // Reset the current editing task
-            saveTasksToLocalStorage();
-        }
-    }
-});
 
 // Filter todos
 function filterTasks(filter) {
@@ -138,6 +175,8 @@ function filterTasks(filter) {
         }
     });
 }
+
+
 
 // Event listeners for filter buttons
 showAllButton.addEventListener('click', () => {
